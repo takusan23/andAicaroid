f02a989a78341c504fadbf2fb04a145edd6ec633
takusan23 <takumu1200@gmail.com>
2025-08-23 17:37:35 +0900

    1.0.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         minSdk = 34
         targetSdk = 36
         versionCode = 1
-        versionName = "1.0"
+        versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
@@ -33,6 +33,9 @@ android {
     }
     buildFeatures {
         compose = true
+    }
+    androidResources {
+        generateLocaleConfig = true
     }
 }
 
